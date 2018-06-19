6f927347c6c298a1faef3a4205a8f341208f3658
Avery-Swank <swankerzzzz@gmail.com>
2018-06-19 11:32:03 -0500

    First Commit by Avery

---
+++ Assets/Navigation-Demo/Navigation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is responsible for translating this object using keyboard input
+// for easy testing purposes
+public class Navigation : MonoBehaviour {
+
+    public float m_currentSpeed = 0.0f;
+    public float m_maxSpeed = 10.0f;
+
+    public Camera m_camera;
+    private Rigidbody m_rigidbody;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("w pressed");
+            m_currentSpeed += .04f;
+            m_rigidbody.AddForceAtPosition(transform.forward * m_currentSpeed, transform.position);
+        }
+
+        Debug.Log(m_currentSpeed);
+        m_currentSpeed = Mathf.Clamp(m_currentSpeed, 0f, m_maxSpeed);   
+	}
+}
---
+++ Assets/Navigation-Demo/NavigationControls.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+// This class is responsible for player controls *FOR TESTING PURPOSES*
+// that includes testing on desktop or in a VR space
+public class NavigationControls : MonoBehaviour {
+
+    private Vector3 m_eulerAngleVelocity = new Vector3(0, 8f, 0);
+    private Rigidbody m_rigidbody;
+
+    private float m_pushForce = 10000f;
+    
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+	void Update()
+    {
+        KeyboardNavigation();
+    }
+
+    private void KeyboardNavigation()
+    {
+        // WASD or arrow Keys:
+        // W - forward
+        // s - backward
+        // a - turn left
+        // d - turn right
+        if (Input.GetKey(KeyCode.W))
+            m_rigidbody.AddForce(-transform.forward * m_pushForce);
+
+        if (Input.GetKey(KeyCode.S))
+            m_rigidbody.AddForce(transform.forward * m_pushForce);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(m_eulerAngleVelocity * -Time.deltaTime);
+            m_rigidbody.MoveRotation(m_rigidbody.rotation * deltaRotation);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(m_eulerAngleVelocity * Time.deltaTime);
+            m_rigidbody.MoveRotation(m_rigidbody.rotation * deltaRotation);
+        }
+    }
+}
